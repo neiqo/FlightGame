d78e375c931241d2fc2bc0d5d69f1023bdee4f61
neil <zaquadq@gmail.com>
2024-01-20 15:30:56 +0800

    Added login page

---
+++ js/login.js
@@ -0,0 +1,93 @@
+// login form
+document.getElementById("login-button").addEventListener("click", function () {
+  const username = document.getElementById("login-username").value;
+  const password = document.getElementById("login-password").value;
+
+  fetch("https://solarquest-81a1.restdb.io/rest/players", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-apikey": "65ab680be8b7cbe259ce52fd",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      let user = data.find(
+        (user) => user.username === username && user.password === password
+      );
+      if (user) {
+        document.getElementById("login-message").textContent =
+          "Login Successful!";
+        sessionStorage.setItem("currentUser", JSON.stringify(user));
+        window.location.href = "homepage.html";
+      } else {
+        document.getElementById("login-message").textContent =
+          "Invalid credentials!";
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      document.getElementById("login-message").textContent =
+        "Login failed due to an error.";
+    });
+});
+
+// registration form
+document
+  .getElementById("register-button")
+  .addEventListener("click", function () {
+    const username = document.getElementById("register-username").value;
+    const password = document.getElementById("register-password").value;
+
+    if (username === "" || password === "") {
+      document.getElementById("register-message").textContent =
+        "Username and password are required!";
+      return;
+    }
+    // user data
+    const newUser = {
+      username: username,
+      password: password,
+      savedata: {
+        current_planet: "earth",
+        level: 1,
+        fuel: 0,
+        title: "Space Cadet",
+      },
+      creationdate: new Date().toISOString(), // current date and time
+    };
+
+    fetch("https://solarquest-81a1.restdb.io/rest/players", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "x-apikey": "65ab680be8b7cbe259ce52fd",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((response) => {
+        if (response.ok) {
+          document.getElementById("register-message").textContent =
+            "Registration successful!";
+          showLogin();
+        } else {
+          throw new Error("Registration failed");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        document.getElementById("register-message").textContent =
+          "Registration failed!";
+      });
+  });
+
+// toggle between registration and login form
+function showRegistration() {
+  document.getElementById("login-form").style.display = "none";
+  document.getElementById("registration-form").style.display = "block";
+}
+
+function showLogin() {
+  document.getElementById("registration-form").style.display = "none";
+  document.getElementById("login-form").style.display = "block";
+}
