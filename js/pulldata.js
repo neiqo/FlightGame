978f497db0957a07d044bed556ef0f0497aeef90
neil <zaquadq@gmail.com>
2024-01-28 18:33:36 +0800

    Removed fetching for moons

--- js/pulldata.js
+++ js/pulldata.js
@@ -56,48 +56,15 @@ function fetchplanet(id) {
     });
 }
 
-function fetchmoon(id) {
-  fetch(
-    `https://en.wikipedia.org/w/api.php?action=query&origin=*&prop=extracts&format=json&titles=${id}_(moon)`
-  )
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Extract relevant content
-      const pages = data.query.pages;
-      const pageId = Object.keys(pages)[0]; // Assuming there's only one page in the response
-      const extract = pages[pageId].extract;
-
-      // Display the extracted content
-      const spansWithIds = extract
-        .split("<span ")
-        .filter((span) => span.includes('id="'));
-
-      for (let i = 0; spansWithIds.length; i++) {
-        let matches = spansWithIds[i].match(/<p>.*?<\/p>/g);
-        console.log(spansWithIds[i]);
-        console.log(matches);
-      }
-      updateContent(extract);
-    })
-    .catch((error) => {
-      console.error("Fetch error:", error);
-    });
-}
-
 function updateContent(content) {
   document.getElementById("content").innerHTML = content;
 }
 
-// fetch data about the player current planet based on usersavedata
+// Fetch data about the player's current planet based on user savedata
 function fetchDataForCurrentPlanet(planetName) {
   if (planets.includes(planetName)) {
     fetchplanet(planetName);
   } else {
-    fetchmoon(planetName);
+    console.error("Invalid planet name:", planetName);
   }
 }
