a6d84139954c49dbdb9184949640cb788549220a
neil <zaquadq@gmail.com>
2024-01-28 16:26:55 +0800

    Edit ID for player save data for better readability

--- js/usersavedata.js
+++ js/usersavedata.js
@@ -2,12 +2,13 @@ window.onload = function () {
   const user = JSON.parse(sessionStorage.getItem("currentUser"));
 
   if (user) {
-    document.getElementById("username-display").textContent = user.username;
-    document.getElementById("current-planet").textContent =
+    document.getElementById("player-username-display").textContent =
+      user.username;
+    document.getElementById("player-current-planet").textContent =
       user.savedata.current_planet;
-    document.getElementById("level").textContent = user.savedata.level;
-    document.getElementById("fuel").textContent = user.savedata.fuel;
-    document.getElementById("title").textContent = user.savedata.title;
+    document.getElementById("player-level").textContent = user.savedata.level;
+    document.getElementById("player-fuel").textContent = user.savedata.fuel;
+    document.getElementById("player-title").textContent = user.savedata.title;
   } else {
     // if got no session data
     window.location.href = "login.html"; // Redirect to login page
