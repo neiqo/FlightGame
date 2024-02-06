4a8a32d528f00c1ff409c49bfbc2e19b487a8428
neil <zaquadq@gmail.com>
2024-02-06 18:46:53 +0800

    Fix api blocked from rate limits

--- js/login.js
+++ js/login.js
@@ -3,11 +3,11 @@ document.getElementById("login-button").addEventListener("click", function () {
   const username = document.getElementById("login-username").value;
   const password = document.getElementById("login-password").value;
 
-  fetch("https://solarquest-2542.restdb.io/rest/players", {
+  fetch("https://solarquest-a3da.restdb.io/rest/players", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "x-apikey": "65b70fc3da76eb22ff969093",
+      "x-apikey": "65c1fc2e72864dcbc2dcc60a",
     },
   })
     .then((response) => response.json())
@@ -57,11 +57,11 @@ document
       creationdate: new Date().toISOString(), // current date and time
     };
 
-    fetch("https://solarquest-2542.restdb.io/rest/players", {
+    fetch("https://solarquest-a3da.restdb.io/rest/players", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "x-apikey": "65b70fc3da76eb22ff969093",
+        "x-apikey": "65c1fc2e72864dcbc2dcc60a",
       },
       body: JSON.stringify(newUser),
     })
