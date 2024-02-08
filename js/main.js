6cae48260723b781440e6fd8dc4332c4bfd054c5
neil <zaquadq@gmail.com>
2024-02-08 21:51:39 +0800

    Removed old scroll button

--- js/main.js
+++ js/main.js
@@ -1,37 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var playButton = document.getElementById("scroll-button");
-
-  playButton.addEventListener("click", function () {
-    var terminal = document.querySelector(".terminal");
-    var terminalBottom = terminal.offsetTop + terminal.offsetHeight;
-
-    // Calculate the distance to scroll
-    var distanceToScroll = terminalBottom - window.scrollY;
-
-    // Set the scroll duration (in milliseconds)
-    var scrollDuration = 1500;
-
-    // Calculate the number of frames based on the duration
-    var frames = Math.ceil(scrollDuration / 16.67); // 60 frames per second
-
-    // Calculate the distance to scroll per frame
-    var distancePerFrame = distanceToScroll / frames;
-
-    // Perform smooth scroll animation
-    function scrollAnimation(currentFrame) {
-      if (currentFrame <= frames) {
-        window.scrollTo(0, window.scrollY + distancePerFrame);
-        currentFrame++;
-        requestAnimationFrame(function () {
-          scrollAnimation(currentFrame);
-        });
-      }
-    }
-
-    // Start the animation
-    scrollAnimation(0);
-  });
-
   function hideInitialPage() {
     var initialPage = document.getElementById("loading-screen");
     initialPage.style.display = "none";
@@ -244,3 +211,30 @@ function wrongAnim() {
   wrongAnim.style.display = "inline";
   wrongAnim.currentTime = 0; // Reset wrong animation to start
 }
+
+// JavaScript
+document.addEventListener("DOMContentLoaded", function () {
+  function hideLandscapeMessage() {
+    var landscapeMessage = document.getElementById("landscape-message");
+    landscapeMessage.style.display = "none";
+  }
+
+  function showLandscapeMessage() {
+    var landscapeMessage = document.getElementById("landscape-message");
+    landscapeMessage.style.display = "block";
+  }
+
+  function checkOrientation() {
+    if (window.innerHeight < window.innerWidth) {
+      hideLandscapeMessage();
+    } else {
+      showLandscapeMessage();
+    }
+  }
+
+  checkOrientation();
+
+  window.addEventListener("orientationchange", function () {
+    checkOrientation();
+  });
+});
